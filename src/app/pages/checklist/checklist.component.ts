1077d90e3e49871c1eb8303acc89100d8cffe0bd
abdelhameedyouxel <ahisham@youxel.com>
2022-03-28 16:41:21 +0200

    Checklist

--- src/app/pages/checklist/checklist.component.ts
+++ src/app/pages/checklist/checklist.component.ts
@@ -1,9 +1,10 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AlertService } from './../../services/alert/alert.service';
 import { HttpService } from './../../services/http/http.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormioEditorOptions } from '@davebaol/angular-formio-editor';
-import { Location } from '@angular/common'
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-checklist',
@@ -13,13 +14,20 @@ import { Location } from '@angular/common'
 export class ChecklistComponent implements OnInit {
   form: any;
   options!: FormioEditorOptions;
-  data:any
-  id!:number;
-  constructor(private route:ActivatedRoute,
+  data: any;
+  id!: number;
+  editMode = '';
+  recordForm!: FormGroup;
+  params: any;
+  latitude!: number;
+  longitude!: number;
+  constructor(private route: ActivatedRoute,
     private http: HttpService,
+    private router: Router,
+    private fb: FormBuilder,
     private location: Location,
-    private alert:AlertService
-    ) {
+    private alert: AlertService
+  ) {
 
 
   }
@@ -68,7 +76,7 @@ export class ChecklistComponent implements OnInit {
           }
         },
         output: {
-          submit: (event) => { this.submission(event) },
+          submit: (event) => { this.submission(event); },
         },
         input: {
           //submission: {
@@ -87,23 +95,218 @@ export class ChecklistComponent implements OnInit {
       }
     };
     this.id = this.route.snapshot.params.id;
-    if(this.id){
+    this.params = this.route.snapshot.queryParams;
+    this.buildForm();
+    if (this.id && this.params.editMode == 'false') {
       this.getById();
+    }
+    if (this.id && this.params.editMode == 'true') {
+      this.editChecklistRecord();
     }
   }
 
 
-  submission(event:any){
-console.log('event',event);
+  buildForm() {
+    console.log('this.params.editMode', this.params.editMode);
+    this.recordForm = this.fb.group({
+      Form_Id: [+this.id],
+      Action: [this.params.editMode == 'true' ? 'Update' : 'Add'],
+      Form_Data: [''],
+      Record_Id: [this.params.Record_Id ? +this.params.Record_Id : 0],
+      FormDataRef: ['', Validators.required],
+      location: ['']
+    });
+  }
+  submission(event: any) {
+    console.log('event', event);
   }
 
   back(): void {
-    this.location.back()
+    this.location.back();
   }
-  getById(){
-    this.http.get('Checklist/GetChecklistById',{Id:this.id}).subscribe((res:any) =>{
-        this.data = res;
-        this.form.components = JSON.parse(res.formControls);
+  getById() {
+    this.http.get('Checklist/GetChecklistById', { Id: this.id }).subscribe((value: any) => {
+      if (value?.gpsRequired) {
+        navigator.geolocation.getCurrentPosition((location) => {
+          this.latitude = location.coords.latitude;
+          this.longitude = location.coords.longitude;
+          this.recordForm.get('location')?.setValue(`${this.latitude},${this.longitude}`);
+        },
+          (err) => {
+            this.location.back();
+            this.alert.error('Please accept to share your location first !');
+          });
+      }
+      this.data = value;
+      this.recordForm.get('Record_Id')?.setValue(0);
+      this.recordForm.get('FormDataRef')?.setValue(value.formDataRef);
+      this.recordForm.get('FormDataRef')?.disable();
+      this.options = {
+        builder: {
+          hideTab: true,
+          hideDisplaySelect: true,
+
+        },
+        json: {
+          hideTab: true,
+          changePanelLocations: ['top', 'bottom'],
+          input: {
+
+          },
+
+        },
+        renderer: {
+          defaultTab: true,
+          submissionPanel: {
+
+            disabled: true,
+            // Whether to initially show full or partial submission. Default to false.
+            fullSubmission: false,
+            // The json editor of the submitted resource.
+            resourceJsonEditor: {
+
+              input: {
+
+              },
+              output: {
+
+              }
+            },
+            schemaJsonEditor: {
+              enabled: false,
+              input: {},
+              output: {}
+            }
+          },
+
+          output: {
+            submit: this.onFormSubmitted.bind(this, event),
+          },
+          input: {
+            submission: {
+
+            },
+            hooks: {
+
+            }
+          }
+        }
+      };
+      this.form.components = JSON.parse(value.formControls);
+    });
+  }
+
+  editChecklistRecord() {
+    this.http.get('ChecklistRecords/EditChecklistRecord', { Form_Id: this.id, Record_Id: this.params.Record_Id }).subscribe((value: any) => {
+      if (value?.gpsRequired) {
+        navigator.geolocation.getCurrentPosition((location) => {
+          this.latitude = location.coords.latitude;
+          this.longitude = location.coords.longitude;
+          this.recordForm.get('location')?.setValue(`${this.latitude},${this.longitude}`);
+        },
+          (err) => {
+            this.location.back();
+            this.alert.error('Please accept to share your location first !');
+          });
+      }
+      this.data = value;
+
+      this.recordForm.get('Record_Id')?.setValue(+this.params.Record_Id);
+      this.recordForm.get('FormDataRef')?.setValue(value.formDataRef);
+      this.recordForm.get('FormDataRef')?.disable();
+      let recordJson = value.record_Json; // data
+      let dataObject = this.deSerialize(JSON.parse(recordJson));
+      this.form.components = JSON.parse(value.form_Layout);
+      this.options = {
+        builder: {
+          hideTab: true,
+          hideDisplaySelect: true,
+
+        },
+        json: {
+          hideTab: true,
+          changePanelLocations: ['top', 'bottom'],
+          input: {
+
+          },
+
+        },
+        renderer: {
+          defaultTab: true,
+          submissionPanel: {
+
+            disabled: true,
+            // Whether to initially show full or partial submission. Default to false.
+            fullSubmission: false,
+            // The json editor of the submitted resource.
+            resourceJsonEditor: {
+
+              input: {
+
+              },
+              output: {
+
+              }
+            },
+            schemaJsonEditor: {
+              enabled: false,
+              input: {},
+              output: {}
+            }
+          },
+
+          output: {
+            submit: this.onFormSubmitted.bind(this, event),
+
+          },
+          input: {
+            // readOnly: this.printed,
+            submission: {
+              data: dataObject
+            },
+            hooks: {
+
+            }
+
+
+          }
+        }
+      };
+    });
+  }
+
+  deSerialize(recordDataArray: any) {
+    let data: any = {};
+    recordDataArray.forEach((item: any) => {
+      data[item?.name] = item?.value;
+    });
+    return data;
+  }
+
+  serializeObj(obj: any) {
+    let result = [];
+    for (var property in obj) {
+      result.push({ name: property, value: obj[property] });
+    }
+    return result;
+  }
+
+  onFormSubmitted(sender: any, eventData: any) {
+
+    let recordData = eventData.data;
+    let serializedData = this.serializeObj(recordData);
+    let formDataStr = JSON.stringify(serializedData);
+
+    let model = { ...this.recordForm.value };
+    model.Form_Data = formDataStr;
+    this.http.post('ChecklistRecords/SaveChecklistRecord', model).subscribe((res: any) => {
+      console.log('res', res);
+      if (res.isPassed) {
+        this.alert.success(res?.message);
+        this.location.back();
+      } else {
+        this.alert.error(res?.message);
+      }
     });
   }
 }
