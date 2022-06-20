11f5d475730ca917d56650185ddb67d12624b1ce
Hossam E. Hassan Ali <hossam_7asebat@hotmail.com>
2022-06-20 17:19:39 +0200

    pup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   version:'1.0',
-  hostAPI:"http://5.189.153.30/api.dynamico/api/"
+  hostAPI:"https://qa-farm.tensorforms.com/api.dynamico/api/"
 };
