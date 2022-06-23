4e6dcc640fa8959dc39d4168affee43a2edbb22f
hadhoud <hadhoud.abdoo@gmail.com>
2022-06-23 17:38:57 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version:'1.2.2',
+  version:'1.2.3',
   hostAPI:"https://qa-farm.tensorforms.com/api.dynamico/api/",
   //hostAPI:"http://5.189.153.30/api.dynamico/api/"
 
