c0d6b218140756dbcf105df14e1c7150fa489256
hadhoud <hadhoud.abdoo@gmail.com>
2022-08-18 17:59:28 +0200

    GetCategoryChecklists paginate

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,12 +1,13 @@
 import { Storage } from '@ionic/storage';
 import { OfflineService } from './../../services/offline/offline.service';
 import { HttpService } from './../../services/http/http.service';
-import { AfterViewInit, Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AlertService } from 'src/app/services/alert/alert.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AccessTypes } from 'src/app/core/enums/access.enum';
+import { IPageInfo } from 'src/app/core/interface/page-info.interface';
 
 @Component({
   selector: 'app-category',
@@ -14,6 +15,17 @@ import { AccessTypes } from 'src/app/core/enums/access.enum';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit, AfterViewInit {
+
+  @HostListener("window:scroll", [])
+  onScroll(): void {
+    if (this.bottomReached() && this.loaded && this.pager.page <= (this.pager.pages - 1)) {
+      // Load Your Data Here
+      this.pager.page += 1;
+      this.loadFromApi()
+    }
+  }
+
+
   @ViewChild('openModal') openModal!: ElementRef;
   @ViewChild('newRecord') newRecord!: TemplateRef<any>;
   items: any = [];
@@ -25,6 +37,10 @@ export class CategoryComponent implements OnInit, AfterViewInit {
   selectedItem: any = {}
   formRef = '';
   accessTypes = AccessTypes;
+
+  pager!: IPageInfo;
+  loaded = true;
+  isLoading = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -49,20 +65,44 @@ export class CategoryComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
+    this.resetPager();
     this.category_Id = this.route.snapshot.params.id;
     this.name = this.route.snapshot.queryParams.name;
     this.formId = this.route.snapshot.queryParams.formId;
   }
-
+  resetPager() {
+    this.pager = {
+      page: 1,
+      pages: 0,
+      pageSize: 10,
+      total: 0,
+    };
+  }
   loadFromApi() {
     this.isOnline ? this.getCategoryChecklists() : ''
   }
   getCategoryChecklists() {
-    this.http.get('Category/GetCategoryChecklists', { categoryId: this.category_Id, userId: JSON.parse(localStorage.getItem('userData') || '{}').userId }).subscribe(async (res) => {
-      this.items = res;
+    this.loaded = false;
+    this.isLoading = true;
+    let params = {
+      categoryId: this.category_Id,
+      userId: JSON.parse(localStorage.getItem('userData') || '{}').userId,
+      formId: this.formId ? this.formId : '',
+      pageIndex: this.pager.page,
+      pageSize: this.pager.pageSize
+    }
+    this.http.get('Category/GetCategoryChecklists', params).subscribe(async (res: any) => {
+      res?.list.map((el: any) => {
+        this.items.push(el);
+      });
+      this.pager.total = res?.total;
+      this.pager.pages = res?.pages;
+      this.loaded = true;
+      this.isLoading = false;
+      // this.items = res;
       let CategoryChecklist = {
         categoryId: +this.category_Id,
-        list: res
+        list: this.items
       };
 
       if (this.formId && res) {
@@ -132,6 +172,12 @@ export class CategoryComponent implements OnInit, AfterViewInit {
         backdrop: 'static',
         size: size
       });
+  }
+  bottomReached(): boolean {
+    return (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 0.5);
+  }
+  scrollToTop() {
+    window.scroll(0, 0);
   }
 
 }
